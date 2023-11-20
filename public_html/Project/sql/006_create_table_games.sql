ebb0a2e0e05eb0518bf4fa9abf496cef2e3168cb
TermyYT <starrinova@gmail.com>
2023-11-19 22:15:48 -0500

    Working on files.

--- public_html/Project/sql/006_create_table_games.sql
+++ public_html/Project/sql/006_create_table_games.sql
@@ -1,4 +1,23 @@
-CREATE TABLE CA_Breeds(
+CREATE TABLE Games (
+    check(originalPrice >= 0),
+    check(discountPrice >= 0),
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `api_id` VARCHAR(255) DEFAULT NULL,
+    `title` VARCHAR(255),
+    `publisherName` VARCHAR(255),
+    `description` TEXT,
+    `effectiveDate` DATETIME,
+    `url` VARCHAR(255),
+    `originalPrice` INT,
+    `discountPrice` INT,
+    `currencyCode` VARCHAR(3),
+    `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`api_id`),
+    UNIQUE KEY (`title`)
+);
+/*CREATE TABLE CA_Breeds(
     `id`         int auto_increment not null,
     `api_id`    VARCHAR(10),
     `name` VARCHAR(30),
@@ -39,4 +58,4 @@ CREATE TABLE CA_Breeds(
     PRIMARY KEY (`id`),
     UNIQUE KEY(`api_id`),
     UNIQUE KEY(`name`)
-)
+)*/
