13b212ef36e238a70f208ceb8a464e719ab7970b
TermyYT <starrinova@gmail.com>
2023-12-01 13:35:09 -0500

    Updated pagination_nav.php and game_helpers.php.

--- partials/pagination_nav.php
+++ partials/pagination_nav.php
@@ -1,13 +1,17 @@
 <?php
+/**
+ * This page is a partial file that renders a page navigation interface.
+ * It contains the Previous button, the page number buttons, and the Next buttons.
+ */
 if (!isset($total)) {
     flash("Note to Dev: The total variable is undefined", "danger");
     error_log("Note to Dev: The total variable is undefined");
     $total = 1;
 }
 
-$per_page = se($_GET, "limit", "10", false);
-$page = se($_GET, "page", 1, false);
-$total_pages = ceil($total / $per_page);
+$per_page = se($_GET, "limit", "10", false); // The default limit set per page is 10 records.
+$page = se($_GET, "page", 1, false); // Starting with page 1.
+$total_pages = ceil($total / $per_page); // Math to determine how many pages should be displayed in the interface.
 //updates or inserts page into query string while persisting anything already present
 function persistQueryString($page)
 {
@@ -31,16 +35,17 @@ function disable_next($page)
 }
 ?>
 
-<nav aria-label="Page navigation example I hope I get changed">
+<nav aria-label="Page Navigation"> <!-- Label changed as wished! -->
     <ul class="pagination justify-content-center">
         <li class="page-item <?php disable_prev(($page - 1)) ?>">
-            <a class="page-link" href="?<?php se(persistQueryString($page - 1)); ?>" tabindex="-1">Previous</a>
+            <a class="page-link" href="?<?php se(persistQueryString($page - 1)); ?>" tabindex="-1">Previous</a> <!-- The Previous button. -->
         </li>
         <?php for ($i = 0; $i < $total_pages; $i++) : ?>
+            <!-- Creating all the page number buttons inbetween. -->
             <li class="page-item <?php set_active($page, $i); ?>"><a class="page-link" href="?<?php se(persistQueryString($i + 1)); ?>"><?php echo ($i + 1); ?></a></li>
         <?php endfor; ?>
         <li class="page-item <?php disable_next($page); ?>">
-            <a class="page-link" href="?<?php se(persistQueryString($page + 1)); ?>">Next</a>
+            <a class="page-link" href="?<?php se(persistQueryString($page + 1)); ?>">Next</a> <!-- The Next button. -->
         </li>
     </ul>
 </nav>
